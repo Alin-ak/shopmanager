664db83923b21cc920c0c0641decb699dc3d63ec
Alin-ak <8954202@qq.com>
2019-02-14 00:07:38 +0800

    配置登录路由“ git commit -m 配置登录路由“

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '@/components/login'
 Vue.use(Router)
 
 export default new Router({
@@ -8,6 +8,11 @@ export default new Router({
     {
       path: '/',
       name: 'home'
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
