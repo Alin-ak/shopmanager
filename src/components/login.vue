6fe693cea6cc6d872fc7a65d932e57e63fd0f171
Alin-ak <8954202@qq.com>
2019-02-14 00:35:44 +0800

    登录页面展示 git commit -m 登录页面展示

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,10 +1,27 @@
 <template>
-    <div>login</div>
+    <div>
+        <el-form label-position="top" label-width="80px" :model="formdata">
+        <el-form-item label="用户名">
+            <el-input v-model="formdata.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+            <el-input v-model="formdata.password"></el-input>
+        </el-form-item>
+        <el-button type="primary">登录</el-button>
+        </el-form>
+    </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      }
+    }
+  }
 }
 </script>
 
